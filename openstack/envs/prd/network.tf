ef3ef5e6e63d5782317ece94bb03bc3fe161b9e1
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2024-12-26 23:35:43 +0900

    allow http from anywhere

--- openstack/envs/prd/network.tf
+++ openstack/envs/prd/network.tf
@@ -54,3 +54,14 @@ resource "openstack_networking_secgroup_rule_v2" "blog_server_ssh" {
   port_range_max   = 22
   remote_ip_prefix = var.cidr_mgmt
 }
+
+resource "openstack_networking_secgroup_rule_v2" "blog_server_http" {
+  security_group_id = openstack_networking_secgroup_v2.blog_server.id
+
+  direction        = "ingress"
+  ethertype        = "IPv4"
+  protocol         = "tcp"
+  port_range_min   = 80
+  port_range_max   = 80
+  remote_ip_prefix = "0.0.0.0/0"
+}
